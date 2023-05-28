7c8dfd19495871437e3f05be239c1947042c8a54
Saviour Eking <106761645+TheSaviourEking@users.noreply.github.com>
2023-05-28 00:24:56 +0000

    adds solutions

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+    else {
+        moveDodgerRight();
+    }
+  });
+  
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 5}px`;
+    }
+  }
+  
+  function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+    
+    if (left < window.innerWidth - dodger.offsetWidth) {
+        dodger.style.left = `${left + 5}px`;
+    }
+  }
